b8d060d8d83ea0a14145cfef210366b9ac129e39
lcc <948200163@qq.com>
2022-08-19 12:03:32 +0800

    fix: ...

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -4,6 +4,7 @@ export default function IndexPage() {
   const a = 1;
   const b = ['airbnb', 'prettier', 'plugin:prettier/recommended'];
 
+  const l = 123;
   return (
     <div>
       <h1 className={classNames('mt-20', 'ml-20')}>
